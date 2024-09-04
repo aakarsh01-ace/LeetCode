54e1dfc011f4f985b5bd5f6ada5bb86e0f3a2f9d
Aakarsh Chaurasia <110169644+aakarsh01-ace@users.noreply.github.com>
2024-09-04 21:23:24 +0530

    Time: 429 ms (53.22%) | Memory: 11.8 MB (75.94%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -13,12 +13,9 @@ public:
                 }
             }
         }
-
         int count = 0;
         for(int i = 2; i<n; i++){
-            if(isPrime[i]){
-                count++;
-            }
+            if(isPrime[i]) count++;
         }
         return count;
     }
